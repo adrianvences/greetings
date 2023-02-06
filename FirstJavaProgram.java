b123c88023e3148de80df016a54ddef627c0181a
adrianvences9820 <adrianvences9820@gmail.com>
2023-02-06 16:29:47 -0500

    first commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,10 @@
+public class FirstJavaProgram {
+  public static void main(String[] args) {
+    String name = "Adrian";
+    String home = "Charlotte, NC";
+    int age = 24;
+
+    System.out.println(String.format( "My name is %s,I am %d years old. My hometown is %s.", name, age, home)); // format: gets filled in auto do not declare
+  }
+  
+}
---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+  public static void main(String[] args) {  // public static void is javas 'function' // main is the method name // perameters each parameters declares what data type it will be
+    System.out.println("Hello World!"); // prints line 
+  }
+}
+
+// anytime we pass a var we have to indicate what type it will be // ex String Number 
+
+//javac Test.java compiles code
+//java Test runs code 
